b9adba75d1078781f7be2fc576fb9b848cbb7fd3
Hailey Boriel <hail01@mit.edu>
2026-01-06 15:48:27 -0500

    small cleanup

--- main.py
+++ main.py
@@ -9,34 +9,6 @@ from scripts.evaluation.evaluator import Evaluator
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# tester = BaselineTester(model_type="siglip", batch_size=1, device=device)
-# a, b = tester.test(test_filepath="data/processed/validate_pairs_ref_10k.parquet")
-
-# print(a)
-# print(b)
-
-# # 1. Build the SigLIP backbone exactly as training
-# tester = BaselineTester(model_type="siglip", batch_size=1, device=device)
-# backbone = tester.model_wrapper  # this exposes encode_text()
-
-# # 2. Build the Siamese model
-# model = SiameseModelPairs(
-#     embedding_dim=768,
-#     projection_dim=768,
-#     backbone=backbone,
-# ).to(device)
-
-# # 3. Load your projector weights (placed in model_utils)
-# state = torch.load("weights/best_model_siglip_pair.pt", map_location=device)
-# model.load_state_dict(state)
-
-# model.eval()
-
-# emb = model.encode(["amazon", "amaz0n"])
-# # print(emb.shape)  # (2, 768)
-# cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-# # print(cos(emb[0:1], emb[1:2]))
-
 def main():
     parser = argparse.ArgumentParser(description='VLM-based text similarity evaluation')
     parser.add_argument('--mode', type=str, 
@@ -112,3 +84,10 @@ def main():
 
 if __name__ == '__main__':
     main() 
+
+    # python Itau-group-2/main.py 
+    # --mode evaluate_saved 
+    # --test_filepath data/processed/validate_pairs_ref_10k.parquet 
+    # --backbone siglip 
+    # --model_weights weights\best_model_siglip_pair.pt 
+    # --plot True
